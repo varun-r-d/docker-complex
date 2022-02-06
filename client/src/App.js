169322cf543d07bac8104ddc5fd4168d3827074b
varun-r-d <varun.ramesh.d@gmail.com>
2022-02-06 22:00:10 +0530

    update app js

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React, via Kubernetes!!
+            Learn React, deployed using Kubernetes!!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
